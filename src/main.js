023eb2f399511cd5c62e60cefad2ff48d33c43ca
xiang <aininaru2020@gmail.com>
2020-10-04 03:25:52 +0200

    引入提示框

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
+import MyHttpServer from '@/plugins/http.js'
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/reset.css'
 import router from './router'
 
 // 使用vue插件
 Vue.use(ElementUI);
+Vue.use(MyHttpServer);
 
 Vue.config.productionTip = false
 
