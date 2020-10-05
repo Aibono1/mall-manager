fd999fa14401d1b7a9a46bba1bd46e9b797939df
xiang <aininaru2020@gmail.com>
2020-10-05 05:48:26 +0200

    添加token验证+退出功能

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -13,7 +13,7 @@
         <el-col :span="18" class="middle"><h3>电商后台管理系统</h3></el-col>
         <el-col :span="2"
           ><div class="grid-content bg-purple">
-            <a class="loginout" href="#">退出</a>
+            <a class="loginout" @click.prevent="handleSignout()" href="#">退出</a>
           </div></el-col
         >
       </el-row>
@@ -98,7 +98,32 @@
 </template>
 
 <script>
-export default {};
+export default {
+
+  beforeCreate(){
+    // 获取token
+    const token = localStorage.getItem('token');
+
+    if (!token){
+      // token没有 -> 转到登陆页面
+      this.$router.push({name:"login"});
+    }
+    // if token有 -> 继续渲染组件
+  },
+
+  methods:{
+    handleSignout(){
+      // 1. 清楚token
+      localStorage.clear();
+      // 2. 提示
+      this.$message.success('退出成功');
+      // 3. 来到login组件
+      this.$router.push({name:'login'})
+
+    }
+  }
+
+};
 </script>
 
 <style>
