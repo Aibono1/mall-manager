e0645a54c717d5150ce77fab7b4ee5ef44e07b7b
xiang <aininaru2020@gmail.com>
2020-10-05 06:14:21 +0200

    用户路由配置

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -22,6 +22,7 @@
       <el-aside class="aside" width="200px">
         <!-- 侧边栏导航 el-menu -->
         <el-menu
+        :router="true"
         :unique-opened="true"
         >
           <el-submenu index="1">
@@ -29,7 +30,7 @@
               <i class="el-icon-location"></i>
               <span>用户管理</span>
             </template>
-              <el-menu-item index="1-1">
+              <el-menu-item index="users">
                 <i class="el-icon-s-grid"></i>
                 <span>用户列表</span>
               </el-menu-item>
@@ -92,7 +93,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
