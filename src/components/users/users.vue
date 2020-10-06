daca8b28b025f983a6209765a9a18329e786d44a
xiang <aininaru2020@gmail.com>
2020-10-06 14:33:55 +0200

    状态和操作

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -16,20 +16,40 @@
         </el-col>
     </el-row>
     <!-- 3. 表格 -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="userlist" style="width: 100%">
         <el-table-column type="index" label="#" width="60">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="80">
+        <el-table-column prop="username" label="姓名" width="80">
         </el-table-column>
-        <el-table-column prop="address" label="邮箱">
+        <el-table-column prop="email" label="邮箱">
         </el-table-column>
-        <el-table-column prop="address" label="电话">
+        <el-table-column prop="mobile" label="电话">
         </el-table-column>
-        <el-table-column prop="address" label="创建日期">
+        <el-table-column label="创建日期">
+            <!-- 如果单元格内显示的内容不是字符串（文本）
+            需要给被显示的内容外层包裹一个template -->
+
+            <!-- template内部要用数据时 设置slot-scope属性
+            该属性的值时要用的数据create_time的数据源userlist -->
+
+            <!-- slot-scope的值userlist其实就是el-table绑定的数据userlist
+            userlist.row -> 数据中的每个对象 -->
+            <template slot-scope="scope">
+                {{scope.row.create_time | fmtdate}}
+            </template>
         </el-table-column>
-        <el-table-column prop="address" label="用户状态">
+        <el-table-column label="用户状态">
+            <template slot-scope="scope">
+                <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
+                </el-switch>
+            </template>
         </el-table-column>
         <el-table-column prop="address" label="操作">
+            <template slot-scope="scope">
+                <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+                <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+                <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+            </template>
         </el-table-column>
     </el-table>
     <!-- 4. 分页 -->
@@ -41,32 +61,47 @@ export default {
     data() {
         return {
             query: '',
-            pagenum:1,
-            pagesize:2,
             // 表格绑定的数据
-            tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }]
+            userlist: [],
+            // 分页相关数据
+            total: -1,
+            pagenum: 1,
+            pagesize: 2
         }
     },
-    created(){
+    created() {
         this.getUserList()
     },
-    methods:{
+    methods: {
         // 获取用户列表的请求
-        async getUserList(){
+        async getUserList() {
             // 需要授权的API，需要在请求头中提供token令牌
             const AUTH_TOKEN = localStorage.getItem('token');
             this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
             const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
             console.log(res);
+            const {
+                meta: {
+                    status,
+                    msg
+                },
+                data: {
+                    users,
+                    total
+                }
+            } = res.data
+            if (status === 200) {
+                // 1. 给表格数据赋值
+                this.userlist = users;
+                // 2. 给total赋值
+                this.total = total;
+                // 3.提示
+                this.$message.success(msg);
+            } else {
+                // 提示
+                this.$message.warning(msg);
+            }
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ import MyHttpServer from '@/plugins/http.js'
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/css/reset.css'
 import router from './router'
+import moment from 'moment'
 
 // 使用vue插件
 Vue.use(ElementUI);
 Vue.use(MyHttpServer);
 
 Vue.config.productionTip = false
+
+// 全局过滤器 - 处理日期
+Vue.filter('fmtdate',(v)=>{
+  return moment(v).format('YYYY-MM-DD');
+})
 
 /* eslint-disable no-new */
 new Vue({
