8faf308e70653e21cbc783bfb1da6ea6de704bbd
xiang <aininaru2020@gmail.com>
2020-10-08 02:01:42 +0200

    动态导航

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -25,70 +25,14 @@
         :router="true"
         :unique-opened="true"
         >
-          <el-submenu index="1">
+          <el-submenu :index="''+item1.order" v-for="(item1,index) in menus" :key="index">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{item1.authName}}</span>
             </template>
-              <el-menu-item index="users">
+              <el-menu-item :index="item2.path" v-for="(item2,index) in item1.children" :key="index">
                 <i class="el-icon-s-grid"></i>
-                <span>用户列表</span>
-              </el-menu-item>
-          </el-submenu>
-          <!-- 2 -->
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-              <el-menu-item index="role">
-                <i class="el-icon-s-grid"></i>
-                <span>角色列表</span>
-              </el-menu-item>
-              <el-menu-item index="rights">
-                <i class="el-icon-s-grid"></i>
-                <span>权限列表</span>
-              </el-menu-item>
-          </el-submenu>
-          <!-- 3 -->
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-              <el-menu-item index="1-1">
-                <i class="el-icon-s-grid"></i>
-                <span>商品列表</span>
-              </el-menu-item>
-              <el-menu-item index="1-1">
-                <i class="el-icon-s-grid"></i>
-                <span>分类参数</span>
-              </el-menu-item>
-              <el-menu-item index="1-1">
-                <i class="el-icon-s-grid"></i>
-                <span>商品分类</span>
-              </el-menu-item>
-          </el-submenu>
-          <!-- 4 -->
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-              <el-menu-item index="1-1">
-                <i class="el-icon-s-grid"></i>
-                <span>订单列表</span>
-              </el-menu-item>
-          </el-submenu>
-          <!-- 5 -->
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-              <el-menu-item index="1-1">
-                <i class="el-icon-s-grid"></i>
-                <span>数据报表</span>
+                <span>{{item2.authName}}</span>
               </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -102,7 +46,11 @@
 
 <script>
 export default {
-
+  data(){
+    return {
+      menus:[]
+    }
+  },
   beforeCreate(){
     // 获取token
     const token = localStorage.getItem('token');
@@ -113,8 +61,15 @@ export default {
     }
     // if token有 -> 继续渲染组件
   },
-
+  created(){
+    this.getMenus()
+  },
   methods:{
+    // 获取导航数据
+    async getMenus(){
+      const res = await this.$http.get(`menus`);
+      this.menus = res.data.data
+    },
     handleSignout(){
       // 1. 清楚token
       localStorage.clear();
--- src/router/index.js
+++ src/router/index.js
@@ -29,8 +29,8 @@ export default new Router({
         path: 'rights', 
         component: Right
       },{
-        name: 'role', 
-        path: 'role', 
+        name: 'roles', 
+        path: 'roles', 
         component: Role
       }]
     }
