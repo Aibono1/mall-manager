cfe2e44ea1e9695934092c3761983be24fa18e2b
xiang <aininaru2020@gmail.com>
2020-11-10 01:51:40 +0100

    完善权限部分

--- src/components/rights/right.vue
+++ src/components/rights/right.vue
@@ -9,17 +9,17 @@
     <my-bread level1="权限管理" level2="权限列表"></my-bread>
 
     <!-- 2. 表格 -->
-    <el-table height="400px" :data="rightlist" border style="width: 100%">
-        <el-table-column type="index" label="#" width="100"> </el-table-column>
-        <el-table-column prop="authName" label="权限名称" width="180"> </el-table-column>
+    <el-table height="400px" :data="rightlist" border stripe>
+        <el-table-column type="index" label="#"> </el-table-column>
+        <el-table-column prop="authName" label="权限名称"> </el-table-column>
         <el-table-column prop="path" label="路径"> </el-table-column>
         <el-table-column label="层级">
             <template slot-scope="scope">
               <span v-if="scope.row.level==='0'">一级</span>
               <span v-if="scope.row.level==='1'">二级</span>
               <span v-if="scope.row.level==='2'">三级</span>
-              </template>
-           </el-table-column>
+            </template>
+        </el-table-column>
     </el-table>
 </el-card>
 </template>
@@ -36,8 +36,11 @@ export default {
     },
     methods: {
         async getRightlist() {
-            const res = await this.$http.get(`rights/list`)
-            this.rightlist = res.data.data
+            const { data:res } = await this.$http.get(`rights/list`)
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取权限列表失败')
+            }
+            this.rightlist = res.data
         }
     }
 };
